d19b1479a50c78c4523568d2a2de64d9caac3be4
Aliqyan <atapia1@bayviewglen.ca>
2017-04-28 15:35:14 -0400

    Creating the Character Hierachy

---
+++ DictatorWars/src/com/bayviewglen/zork/Enemy.java
@@ -0,0 +1,10 @@
+package com.bayviewglen.zork;
+
+public abstract class Enemy extends Character{
+
+	public Enemy(int healthPoints, int speed, int strength) {
+		super(healthPoints, speed, strength);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ DictatorWars/src/com/bayviewglen/zork/Player.java
@@ -0,0 +1,11 @@
+package com.bayviewglen.zork;
+
+public class Player extends Character {
+
+	public Player(int healthPoints, int speed, int strength) {
+		super(healthPoints, speed, strength);
+	}
+	
+
+	
+}
