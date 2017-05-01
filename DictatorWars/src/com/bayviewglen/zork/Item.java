f94e841bd3342791f8a1aafc0bdcce88c13d0e4f
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-05-01 09:45:15 -0400

    Add pickp method in plkayer

--- DictatorWars/src/com/bayviewglen/zork/Item.java
+++ DictatorWars/src/com/bayviewglen/zork/Item.java
@@ -2,9 +2,8 @@ package com.bayviewglen.zork;
 
 public abstract class Item {
 	
-	
 	public void pickUp(){
-		
+		//adds to inventory
 	}
 	
 }
--- DictatorWars/src/com/bayviewglen/zork/Player.java
+++ DictatorWars/src/com/bayviewglen/zork/Player.java
@@ -1,9 +1,18 @@
 package com.bayviewglen.zork;
 
-public class Player extends Character {
+import java.util.ArrayList;
 
+public class Player extends Character {
+	ArrayList<Item> inventory = new ArrayList<Item>();
+	
+	
 	public Player(int healthPoints, int speed, int strength) {
 		super(healthPoints, speed, strength);
+	}
+	
+	public void pickUp(Item item){
+		inventory.add(item);
+		//take item removed from room and adds to inventory
 	}
 	
 
