645907d3e102b412c8a5e0bd9001ec9ea765383a
atapia1 <atapia1@atapia1sp3.bayviewglen.ca>
2017-05-01 14:56:08 -0400

    Enemies are read from the file and put in rooms

--- DictatorWars/src/com/bayviewglen/zork/Boss.java
+++ DictatorWars/src/com/bayviewglen/zork/Boss.java
@@ -1,7 +1,7 @@
 package com.bayviewglen.zork;
 
 public class Boss extends Enemy {
-
+	//small change
 	public Boss(int healthPoints, int speed, int strength, String name) {
 		super(healthPoints, speed, strength, name);
 		// TODO Auto-generated constructor stub
