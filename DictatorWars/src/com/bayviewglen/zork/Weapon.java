577f038cfcc17d4f4978c5dbd175b3c778c36709
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-05-01 13:00:37 -0400

    Weapons adding funucytionality

--- DictatorWars/src/com/bayviewglen/zork/Weapon.java
+++ DictatorWars/src/com/bayviewglen/zork/Weapon.java
@@ -4,8 +4,10 @@ package com.bayviewglen.zork;
 public class Weapon extends Item{
 	private int damage;
 	
-	public Weapon(int weight, String name){
+	public Weapon(int weight, String name, int damage){
 		super(weight, name);
+		this.damage = damage;
+		
 	}
 	
 	public void attack(Enemy enemy, Player player, Weapon weapon){
@@ -16,8 +18,5 @@ public class Weapon extends Item{
 		return damage;
 	}
 
-	public void setDamage(int damage) {
-		this.damage = damage;
-	}
 	
 }
