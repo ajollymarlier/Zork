bfd7acc5cb2d68309084518469a5f0eaa7795a4d
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-05-02 14:03:16 -0400

    Amino Acids pt 3

--- DictatorWars/src/com/bayviewglen/zork/Ammo.java
+++ DictatorWars/src/com/bayviewglen/zork/Ammo.java
@@ -1,5 +1,19 @@
 package com.bayviewglen.zork;
 
-public class Ammo {
+public class Ammo extends Item{
+	private int amt;
 	
+	public Ammo(int weight, String name, int amt) {
+		super(weight, name);
+		this.amt = amt;
+	}
+	
+	public void setAmt(int amt){
+		this.amt = amt;
+	}
+	
+	public int getAmt(){
+		return amt;
+	}
+	 
 }
