10016223ba86203d47ff129695f3fba6ee7df3fa
drossos <drossos@drossossp3.bayviewglen.ca>
2017-05-02 13:29:59 -0400

    Added a third word to parser
    Also fixed picking up items.

--- DictatorWars/src/com/bayviewglen/zork/Player.java
+++ DictatorWars/src/com/bayviewglen/zork/Player.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Player extends Character {
 	private ArrayList<Item> inventory = new ArrayList<Item>();
 	private int weightCarried = 0;
-	private final int MAX_WEIGHT = (int) Math.sqrt((getStrength() * 4));
+	//can change max weight, just sorta default TODO
+	private final int MAX_WEIGHT = super.getStrength() * 10; 
 	//test
 	//TODO This max weight will be changed to a variable based on character attributes
 	
@@ -14,14 +15,16 @@ public class Player extends Character {
 		super(healthPoints, speed, strength);
 	}
 	
-	public void pickUp(Item item, Room room){
+	public boolean pickUp(Item item, Room room){
 		weightCarried += item.getWeight();
 		
 		if (weightCarried <= MAX_WEIGHT){
 			inventory.add(item);
+			return true;
 		}else{
 			weightCarried -= item.getWeight();
 			room.addRoomItems(item);
+			return false;
 			//will work when parameters in game class are fixed
 		}	
 	}
