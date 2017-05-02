86eacb9f63372052ea2ae817672585e38dd01699
Arun Jolly Marlier <ajollymarlier@bayviewglen.ca>
2017-05-02 14:10:20 -0400

    Add files via upload
    Amino Acids pt 3

--- DictatorWars/src/com/bayviewglen/zork/Player.java
+++ DictatorWars/src/com/bayviewglen/zork/Player.java
@@ -4,49 +4,53 @@ import java.util.ArrayList;
 
 public class Player extends Character {
 	private ArrayList<Item> inventory = new ArrayList<Item>();
+	private ArrayList<Ammo> ammoBag = new ArrayList<Ammo>();
 	private int weightCarried = 0;
-	//can change max weight, just sorta default TODO
-	private final int MAX_WEIGHT = super.getStrength() * 10; 
-	//test
-	//TODO This max weight will be changed to a variable based on character attributes
+
+	// can change max weight, just sorta default TODO
+	private final int MAX_WEIGHT = super.getStrength() * 10;
 	
-	
+	// TODO This max weight will be changed to a variable based on character
+	// attributes
+
 	public Player(int healthPoints, int speed, int strength) {
 		super(healthPoints, speed, strength);
 	}
-	
-	public boolean pickUp(Item item, Room room){
+
+	public boolean pickUp(Item item, Room room) {
 		weightCarried += item.getWeight();
-		
-		if (weightCarried <= MAX_WEIGHT){
+
+		if (weightCarried <= MAX_WEIGHT && item instanceof Ammo) {
+			//TODO need to increase amt of ammo object if it is already present
+			ammoBag.add((Ammo) item);
+			return true;
+			
+		} else if (weightCarried <= MAX_WEIGHT) {
 			inventory.add(item);
 			return true;
-		}else{
+			
+		} else {
 			weightCarried -= item.getWeight();
 			room.addRoomItems(item);
 			return false;
-			//will work when parameters in game class are fixed
-		}	
+			// will work when parameters in game class are fixed
+		}
 	}
-	
-	//TODO this sort of works... Theoretically
-	public void drop(Item item, Room room){
+
+	// TODO this sort of works... Theoretically
+	public void drop(Item item, Room room) {
 		weightCarried -= item.getWeight();
 		room.addRoomItems(findItem(item));
 	}
 
-	//TODO this too
+	// TODO this too
 	private Item findItem(Item item) {
-		for(int i = 0; i < inventory.size(); i++){
-			if (inventory.get(i).equals(item)){
+		for (int i = 0; i < inventory.size(); i++) {
+			if (inventory.get(i).equals(item)) {
 				return inventory.remove(i);
 			}
 		}
 		return null;
 	}
-	
-	
-	
 
-	
 }
