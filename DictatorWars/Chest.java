044b9ab03dcf62bc65043d795a99b6a7e7c9adb6
drossos <drossos@bayviewglen.ca>
2017-05-02 13:45:20 -0400

    Add files via upload

---
+++ DictatorWars/Chest.java
@@ -0,0 +1,23 @@
+package com.bayviewglen.zork;
+
+public class Chest extends Item implements Lockable {
+
+	@Override
+	public boolean isLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean unlock() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean lock() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
