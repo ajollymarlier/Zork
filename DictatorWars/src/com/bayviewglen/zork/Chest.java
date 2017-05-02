77b5ffc498ddea138712252d15a7594321d9dcf1
drossos <drossos@drossossp3.bayviewglen.ca>
2017-05-02 14:13:31 -0400

    Chest constructor still not fucntion chest

--- DictatorWars/src/com/bayviewglen/zork/Chest.java
+++ DictatorWars/src/com/bayviewglen/zork/Chest.java
@@ -1,21 +1,28 @@
 package com.bayviewglen.zork;
 
 public class Chest extends Item implements Lockable {
-
-	@Override
+	private boolean locked;
+	private Item key;
+	
+	//TODO MAKE THE KEYS WORK TO UNLOCK CHEST
+	public Chest (int weight, String name, boolean locked, Item key){
+		super(weight,name);
+		this.locked = locked;
+		this.key = key;
+	}
+	
+	
 	public boolean isLocked() {
+		return locked;
+	}
+
+	//returns true if ends up locked
+	public boolean unlock(Item key) {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	@Override
-	public boolean unlock() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean lock() {
+	//returns true if it ends up locked
+	public boolean lock(Item key){
 		// TODO Auto-generated method stub
 		return false;
 	}
