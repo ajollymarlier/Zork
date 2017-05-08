8977271ae2c162683b8a42bf9888fbdd84bb6660
Arun Jolly Marlier <ajollymarlier@bayviewglen.ca>
2017-05-08 14:11:21 -0400

    Add files via upload

---
+++ DictatorWars/src/com/bayviewglen/zork/Inventory.java
@@ -0,0 +1,12 @@
+package com.bayviewglen.zork;
+
+import java.util.ArrayList;
+
+public class Inventory {
+	public ArrayList<Item> inv;
+	
+	public Inventory(){
+		inv = new ArrayList<Item>();
+	}
+	
+}
