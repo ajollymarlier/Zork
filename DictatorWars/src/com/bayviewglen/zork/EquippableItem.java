43c56839a3f9bf6a71618fadd904794127c19eb1
ajollymarlier <ajollymarlier@ajmarliersp3.bayviewglen.ca>
2017-05-10 15:31:28 -0400

    Added stuff in equippable item

--- DictatorWars/src/com/bayviewglen/zork/EquippableItem.java
+++ DictatorWars/src/com/bayviewglen/zork/EquippableItem.java
@@ -1,5 +1,47 @@
 package com.bayviewglen.zork;
 
-public class EquippableItem {
-
+public class EquippableItem extends Item{
+	private boolean equipped;
+	private int healthBoost;
+	private int defenseBoost;
+	private int speedBoost;
+	private int strengthBoost;
+	
+	//TODO add defense attribute in player class
+	public EquippableItem(int weight, String name, int healthBoost, int defenseBoost, int speedBoost, int strengthBoost){
+		super(weight, name);
+		this.equipped = false;
+		this.healthBoost = healthBoost;
+		this.defenseBoost = defenseBoost;
+		this.speedBoost = speedBoost;
+		this.strengthBoost = strengthBoost;
+	} 
+	
+	public void setEquipped(boolean is){
+		equipped = is;
+	}
+	
+	public boolean getEquipped(){
+		return equipped;
+	}
+	
+	public int getHealthBoost(){
+		return healthBoost;
+	}
+	
+	public int getDefenseBoost(){
+		return defenseBoost;
+	}
+	
+	public int getSpeedBoost(){
+		return speedBoost;
+	}
+	
+	public int getStrengthBoost(){
+		return strengthBoost;
+	}
+	//TODO edit equip method in player class
+	
+	
+	
 }
