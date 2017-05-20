083dcf3996741202631d0e1c5ef473fdee5e7700
ajollymarlier <ajollymarlier@bayviewglen.ca>
2017-05-20 14:03:37 -0400

    boi

--- DictatorWars/src/com/bayviewglen/zork/EffectItem.java
+++ DictatorWars/src/com/bayviewglen/zork/EffectItem.java
@@ -3,6 +3,8 @@ package com.bayviewglen.zork;
 public class EffectItem extends ConsumableItem{
 	private String type;
 	
+	//This is a test
+	
 	public EffectItem(int weight, String name, String type){
 		super(weight, name);
 		this.type = type;
